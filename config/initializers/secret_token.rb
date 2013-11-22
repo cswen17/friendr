5b27bc3a52ccd9ffecd8ad40e891aaa946f81247
cswen17 <cswen@andrew.cmu.edu>
2013-11-21 23:19:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendr::Application.config.secret_token = '025ebad82eaf631c9d066900930227e19f8fd1077a8b793bf56cc247080cfc30492eb43d7f273daef0d6899727d5f7d8fa04e454d502473b495d8c83df5225a9'
